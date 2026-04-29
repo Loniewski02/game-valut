48c9d1b4c721c20511dc6dd0dbe9903f54c8f42a
Loniewski02 <miloszloniewski02@gmail.com>
2026-04-29 19:11:01 +0200

    changed item width

--- app/components/games/controls/GamesControlsList.tsx
+++ app/components/games/controls/GamesControlsList.tsx
@@ -52,7 +52,7 @@ const GamesControlsList: React.FC<Props> = ({ name, className, items, onFilter, 
                   <BiCheck className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-lg text-primary" />
                 )}
               </span>
-              {item}
+              <span className="w-24 text-left">{item}</span>
             </button>
           );
         })}
