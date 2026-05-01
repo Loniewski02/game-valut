c504f347410bc367ae3fd38b7e7540394a70ad37
Loniewski02 <miloszloniewski02@gmail.com>
2026-05-01 14:41:18 +0200

    added key prop on features

--- app/components/home/Features.tsx
+++ app/components/home/Features.tsx
@@ -16,7 +16,7 @@ const Features = () => {
           {features.map((feature) => {
             const Icon = feature.icon;
             return (
-              <div className="flex max-w-lg items-center gap-6 rounded-2xl bg-White px-6 py-4">
+              <div key={feature.id} className="flex max-w-lg items-center gap-6 rounded-2xl bg-White px-6 py-4">
                 <div className="rounded-2xl bg-LightGray p-5">
                   <Icon className="text-4xl" />
                 </div>
